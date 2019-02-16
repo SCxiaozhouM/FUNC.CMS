6b3c76050fc941ee0e33436ae23ca5b3b72f4452
ZWL <ZWL@DESKTOP-85B8FNI>
2019-02-16 21:27:40 +0800

    权限

--- src/HS.CMS/HS.Web/EntityAuthorizeAttribute.cs
+++ src/HS.CMS/HS.Web/EntityAuthorizeAttribute.cs
@@ -1,7 +1,12 @@
 ﻿using HS.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace HS.Web
@@ -26,9 +31,72 @@ namespace HS.Web
 
             Permission = permission;
         }
-        public void OnAuthorization(AuthorizationFilterContext context)
+        public void OnAuthorization(AuthorizationFilterContext filterContext)
         {
-            throw new NotImplementedException();
+            /*
+             * 验证范围：
+             * 1，区域下的所有控制器
+             * 2，所有带有EntityAuthorize特性的控制器或动作
+             */
+            var act = filterContext.ActionDescriptor;
+            var ctrl = (ControllerActionDescriptor)act;
+
+            // 允许匿名访问时，直接跳过检查
+            if (
+                ctrl.MethodInfo.IsDefined(typeof(AllowAnonymousAttribute)) ||
+                ctrl.ControllerTypeInfo.IsDefined(typeof(AllowAnonymousAttribute))) return;
+
+            // 如果控制器或者Action放有该特性，则跳过全局
+            var hasAtt =
+                ctrl.MethodInfo.IsDefined(typeof(EntityAuthorizeAttribute), true) ||
+                ctrl.ControllerTypeInfo.IsDefined(typeof(EntityAuthorizeAttribute));
+
+            if (IsGlobal && hasAtt) return;
+
+            
+
+            // 如果已经处理过，就不处理了
+            if (filterContext.Result != null) return;
+
+            if (!AuthorizeCore(filterContext.HttpContext))
+            {
+                HandleUnauthorizedRequest(filterContext);
+            }
+        }
+
+        /// <summary>授权核心</summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        protected Boolean AuthorizeCore(Microsoft.AspNetCore.Http.HttpContext httpContext)
+        {
+
+
+            // 判断当前登录用户
+            var user = "";
+            if (user == null) return false;
+
+            // 判断权限
+            return false;
+        }
+
+        /// <summary>未认证请求</summary>
+        /// <param name="filterContext"></param>
+        protected void HandleUnauthorizedRequest(AuthorizationFilterContext filterContext)
+        {
+            // 来到这里，有可能没登录，有可能没权限
+            var prv = "";
+            if (prv == null)
+            {
+                var retUrl = filterContext.HttpContext.Request.GetEncodedPathAndQuery();
+                //.Host.ToString();//.Url?.PathAndQuery;
+
+                var rurl = "~/Admin/User/Login";//.AppendReturn(retUrl);
+                filterContext.Result = new RedirectResult(rurl);
+            }
+            else
+            {
+               // filterContext.Result = filterContext.NoPermission(Permission);
+            }
         }
     }
 }
